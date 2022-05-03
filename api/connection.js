751f4b18c98aa718858e9142a76568f0689bd6d5
hyve9 <afb8252@nyu.edu>
2022-05-03 01:56:44 -0400

    Add API backend

---
+++ api/connection.js
@@ -0,0 +1,73 @@
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "",
+    database: "disco"
+})
+
+const getFilters = (req, res) => {
+  pool.query(`Select * from filters`, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).json(result.rows);
+  })
+}
+
+const getFiltersByFreq = (req, res) => {
+  pool.query(`Select * from filters where freq=${req.params.freq}`, (err, result) => {
+    if (err) {
+      throw err
+    }
+    res.status(200).json(result.rows)
+  })
+}
+
+const createFilter = (req, res) => {
+  const filter = req.body;
+  let insertQuery = `insert into filters(freq, gain)
+  values(${filter.freq}, '${filter.gain}')`
+
+  pool.query(insertQuery, (err, result)=>{
+    if (err) {
+      throw err
+    }
+    res.status(200).json(result.rows)
+  })
+}
+
+const updateFilter = (req, res) => {
+  let filter = req.body;
+  let updateQuery = `update filters
+    set gain = '${filter.gain}'
+    where freq = ${filter.freq}`
+
+  pool.query(updateQuery, (err, result)=>{
+    if (err) {
+      throw err
+    }
+    res.status(200).json(result.rows)
+  })
+}
+
+const deleteFilter = (req, res) => {
+  let insertQuery = `delete from filters where freq=${req.params.freq}`
+
+  pool.query(insertQuery, (err, result) => {
+    if (err) {
+      throw err
+    }
+    res.status(200).json(result.rows)
+  })
+}
+
+module.exports = {
+  getFilters,
+  getFiltersByFreq,
+  createFilter,
+  updateFilter,
+  deleteFilter
+}
